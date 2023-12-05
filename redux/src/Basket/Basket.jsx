ead90bc5661585b97db42ab333cbe233242c4f14
IlkinAkhmed <tu7mp4w99@code.edu.az>
2023-12-06 00:01:03 +0400

    basket done

--- redux/src/Basket/Basket.jsx
+++ redux/src/Basket/Basket.jsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { addbasket,increase,decrease,remove } from "./BasketSlice";
+import { addbasket, increase, decrease, remove } from "./BasketSlice";
 
 function Basket() {
   const [data, setData] = useState([]);
-  
+
   useEffect(() => {
     fetch("https://northwind.vercel.app/api/categories")
       .then((res) => res.json())
@@ -14,34 +14,42 @@ function Basket() {
   const basketArr = useSelector((state) => state.basket.value);
   const dispatch = useDispatch();
 
+
   return (
     <>
-      <div style={{backgroundColor:'red',color:'white'}} className="basket">
+      <div style={{ backgroundColor: 'gray', color: 'white' }} className="basket">
+        <h1>BASKET</h1>
         {basketArr &&
           basketArr.map((x) => (
-            <>
+            <div key={x.id}>
               <hr />
-              <ul key={x.id}>
+              <ul>
                 <li>{x.name}</li>
                 <li>{x.description}</li>
-                <button onClick={()=>dispatch(increase(x))}>+</button>
+                <button onClick={() => dispatch(increase(x))}>+</button>
                 <span>{x.count}</span>
-                <button onClick={()=>dispatch(decrease(x))}>-</button>
-                <button onClick={()=>dispatch(remove(x))}>delete</button>
+                <button onClick={() => dispatch(decrease(x))}>-</button>
+                <button onClick={() => dispatch(remove(x))}>delete</button>
               </ul>
               <hr />
-            </>
+            </div>
           ))}
       </div>
-      {data &&
-        data.map((x) => (
-          <ul style={{backgroundColor:'aqua',color:'black'}} key={x.id}>
-            <li>{x.name}</li>
-            <li>{x.description}</li>
-            <button onClick={() => dispatch(addbasket({...x,count:1}))}>add</button>
-            <hr />
-          </ul>
-        ))}
+      <div style={{ backgroundColor: 'black', color: 'white' }}>
+        <h1>SHOP</h1>
+        {data &&
+          data.map((x) => (
+            <>
+              <ul style={{ backgroundColor: 'black', color: 'white' }} key={x.id}>
+                <li>{x.name}</li>
+                <li>{x.description}</li>
+                <button onClick={() => dispatch(addbasket({ ...x, count: 1 }))}>add</button>
+                <hr />
+              </ul>
+            </>
+          ))}
+
+      </div>
     </>
   );
 }
--- redux/src/Basket/BasketSlice.jsx
+++ redux/src/Basket/BasketSlice.jsx
@@ -1,38 +1,54 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+
+
+
+const initialBasket = localStorage.getItem("basketArr") ? JSON.parse(localStorage.getItem("basketArr")) : [];
+
 const initialState = {
-  value: [],
+  value: initialBasket,
 };
+
 
 export const Basketslice = createSlice({
   name: "basket",
   initialState,
+
   reducers: {
     addbasket: (state, action) => {
       const find = state.value.find((x) => x.id === action.payload.id);
       if (find) {
         find.count++;
-        return;
       } else {
         state.value.push(action.payload);
       }
+      localStorage.setItem("basketArr", JSON.stringify(state.value));
     },
     increase: (state, action) => {
       const find = state.value.find((x) => x.id === action.payload.id);
       if (find) {
         find.count++;
       }
+      localStorage.setItem("basketArr", JSON.stringify(state.value));
     },
     decrease: (state, action) => {
       const find = state.value.find((x) => x.id === action.payload.id);
       if (find) {
         find.count--;
+        if (find.count === 0) {
+          state.value = state.value.filter((x) => x.id !== action.payload.id);
+        }
+        localStorage.setItem("basketArr", JSON.stringify(state.value));
       }
     },
     remove: (state, action) => {
-        state.value=state.value.filter(x=>x.id!==action.payload.id)
+      state.value = state.value.filter((x) => x.id !== action.payload.id);
+      localStorage.setItem("basketArr", JSON.stringify(state.value));
     },
   },
 });
-export const { addbasket,increase,decrease,remove } = Basketslice.actions;
+
+
+
+export const { addbasket, increase, decrease, remove } = Basketslice.actions;
 export default Basketslice.reducer;
